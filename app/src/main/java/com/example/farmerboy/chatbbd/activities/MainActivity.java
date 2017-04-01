d1895d76fa70c80a878b2203841109b31023f022
baolinux <baot3bk@gmail.com>
2017-04-01 09:32:56 +0700

    New change MMMM

--- app/src/main/java/com/example/farmerboy/chatbbd/activities/MainActivity.java
+++ app/src/main/java/com/example/farmerboy/chatbbd/activities/MainActivity.java
@@ -15,5 +15,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        addControl();
+
+    }
+
+    private void addControl() {
+
+
     }
 }
